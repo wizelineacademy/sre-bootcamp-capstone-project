ec7a5842f791c79e8e800ee4584960bc2e2aa304
beltranbot <beltranbot@gmail.com>
2021-09-17 01:16:25 -0500

    more missing semicolons

--- node/validators/maskValidator.js
+++ node/validators/maskValidator.js
@@ -3,10 +3,10 @@ const maskService = require('../services/maskService');
 module.exports = (req, res, next) => {
   const mask = req.params.mask ? req.params.mask : false;
   if (!mask) {
-    return res.send(422, { mask: 'mask is required' })
+    return res.send(422, { mask: 'mask is required' });
   }
   if (!maskService.isValidMask(mask)) {
-    return res.send(422, { mask: 'provided value is not a valid subnet mask' })
+    return res.send(422, { mask: 'provided value is not a valid subnet mask' });
   }
-  next()
+  next();
 }
