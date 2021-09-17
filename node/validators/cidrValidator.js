5c45e66ca44f8ae5c25c1b6738120a9ec3362ef9
beltranbot <beltranbot@gmail.com>
2021-09-17 01:15:46 -0500

    added missing semicolons

--- node/validators/cidrValidator.js
+++ node/validators/cidrValidator.js
@@ -3,11 +3,11 @@ const cidrService = require('../services/cidrService');
 module.exports = (req, res, next) => {
   const cidr = req.params.cidr ? req.params.cidr : false;
   if (!cidr) {
-    return res.send(422, { cidr: 'cidr is required' })
+    return res.send(422, { cidr: 'cidr is required' });
   }
   if (!cidrService.isAValidCidr(cidr)) {
-    return res.send(422, { cidr: 'cidr must be a numeric value between 1 and 32' })
+    return res.send(422, { cidr: 'cidr must be a numeric value between 1 and 32' });
   }
-  req.params.cidr = +req.params.cidr
+  req.params.cidr = +req.params.cidr;
   next()
 }
