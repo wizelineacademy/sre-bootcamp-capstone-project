bbd47f8895c5cc075fbb76235f02b07f92dc2344
beltranbot <beltranbot@gmail.com>
2021-09-19 15:54:11 -0500

    added healthControllerTest

---
+++ node/test/controllers/healthControllerTest.js
@@ -0,0 +1,22 @@
+const { describe, it } = require('mocha');
+const sinon = require('sinon')
+
+const healthController = require('../../controllers/healthController');
+
+describe('healthController.getHealth', function () {
+  it('should return OK if called', function () {
+    const res = {
+      send: function () { }
+    };
+    const next = {
+      next: function () {}
+    };
+    const resMock = sinon.mock(res);
+    const nextMock = sinon.mock(next);
+    resMock.expects('send').once().withArgs("OK");
+    nextMock.expects("next").once().withArgs();
+    healthController.getHealth(null, res, next.next)
+    resMock.verify();
+    nextMock.verify();
+  });
+});
